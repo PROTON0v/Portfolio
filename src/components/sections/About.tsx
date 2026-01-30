8b89eb8420697e73dbcf5a27e11486f6544a8980
PROTON0v <hma02600@gmail.com>
2026-01-30 12:19:12 +0100

    added me pfp

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -13,9 +13,9 @@ const About = memo(function About() {
       transition={{ duration: 0.5, delay: 0.2 }}
       viewport={{ once: true }}
       id="about-section"
-      className="min-h-screen pb-5 md:pb-20 pt-20 sm:pt-24 lg:pt-32 px-4 sm:px-6 lg:px-8   "
+      className="min-h-screen pb-5 md:pb-20 pt-20 sm:pt-24 lg:pt-32 px-4 sm:px-6 lg:px-8"
     >
-      <div className="max-w-7xl mx-auto">
+      <div className="w-full max-w-7xl mx-auto">
         {/* About Me Section */}
         <div className="mb-20 sm:mb-24 lg:mb-32">
           {/* Section Header */}
@@ -28,16 +28,16 @@ const About = memo(function About() {
             className="text-center mb-12 sm:mb-16 lg:mb-20"
           >
             <div
-              className={`text-sm sm:text-base opacity-60 tracking-wide mb-4  text-muted-foreground `}
+              className={` text-sm sm:text-base opacity-60 tracking-wide mb-4  text-muted-foreground `}
             >
               — About Me
             </div>
-            <h2 className="text-3xl sm:text-4xl lg:text-5xl mb-6">
+            <h2 className="text-2xl sm:text-4xl lg:text-5xl mb-6">
               Building Digital <span className="font-light">Experiences</span>
             </h2>
 
             <p
-              className={`text-lg sm:text-xl max-w-3xl mx-auto leading-relaxed  text-muted-foreground`}
+              className={`w-80 sm:w-auto text-lg sm:text-xl max-w-3xl mx-auto leading-relaxed  text-muted-foreground `}
             >
               I craft modern, responsive web applications focused on
               performance, scalability, and clean design.
@@ -52,7 +52,7 @@ const About = memo(function About() {
             className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16 "
           >
             {/* Photo Placeholder */}
-            <div className="flex justify-around   ">
+            <div className="flex justify-around ">
               <div className="relative self-center  ">
                 <div
                   className={`relative  rounded-2xl  bg-card border border-border backdrop-blur-sm`}
@@ -61,7 +61,7 @@ const About = memo(function About() {
                     <Image
                       width={400}
                       height={400}
-                      src="/pfp.jpg"
+                      src="/me.jpg"
                       alt="About Me Portrait"
                       className="w-full h-full object-cover"
                       loading="lazy"
@@ -75,41 +75,38 @@ const About = memo(function About() {
             </div>
 
             <div
-              className={`h-60 w-px hidden lg:block    absolute -bottom-[1000px] bg-gradient-to-b from-transparent via-border to-transparent
+              className={`h-60 w-px hidden lg:block  absolute -bottom-[1000px] bg-gradient-to-b from-transparent via-border to-transparent
   }`}
             />
 
             {/* About Content */}
-            <div className="space-y-6 sm:space-y-8">
-              <div>
-                <h3 className="text-xl sm:text-2xl mb-4 sm:mb-6">My Journey</h3>
-                <div className="space-y-4 sm:space-y-6">
-                  <p className={`leading-relaxed  text-muted-foreground`}>
-                    I started coding out of curiosity — wanting to understand
-                    how digital products work. That curiosity turned into a
-                    passion for building sleek, fast, and user-focused
-                    applications.
-                  </p>
+            <div className=" space-y-6 sm:space-y-8 mx-auto">
+              <h3 className="text-xl sm:text-2xl mb-4 sm:mb-6">My Journey</h3>
+              <div className="space-y-4 sm:space-y-6 w-80 sm:w-auto">
+                <p className={`leading-relaxed  text-muted-foreground`}>
+                  I started coding out of curiosity — wanting to understand how
+                  digital products work. That curiosity turned into a passion
+                  for building sleek, fast, and user-focused applications.
+                </p>
 
-                  <p
-                    className={`leading-relaxed  
+                <p
+                  className={`leading-relaxed  
     text-muted-foreground `}
-                  >
-                    Now, I specialize in building scalable full-stack web apps
-                    with React, Next.js, Node.js, and PostgreSQL. My focus is on
-                    performance, clean code, and real-world usability.
-                  </p>
+                >
+                  Now, I specialize in building scalable full-stack web apps
+                  with React, Next.js, Node.js, and PostgreSQL. My focus is on
+                  performance, clean code, and real-world usability.
+                </p>
 
-                  <p
-                    className={`leading-relaxed 
+                <p
+                  className={`leading-relaxed 
      text-muted-foreground
       `}
-                  >
-                    Outside of work, I explore emerging tools, contribute to
-                    open-source projects, and share what I learn with other
-                    developers.
-                  </p>
-                </div>
+                >
+                  Outside of work, I explore emerging tools, contribute to
+                  open-source projects, and share what I learn with other
+                  developers.
+                </p>
               </div>
             </div>
           </motion.div>
@@ -156,140 +153,124 @@ const About = memo(function About() {
             ></div>
 
             <div className="flex brightness-90">
-            {/* Technology Grid */}
-            <motion.div
-              whileInView={{ opacity: 1 }}
-              initial={{ x: 0 }}
-              viewport={{ once: true}}
-              animate={{ x: "-100%" }}
-              transition={{
-                repeat: Infinity,
-                duration: 30,
-                ease: "linear",
-              }}
-              className="flex"
-            >
-              {repeatTechnologies.map((tech, index) => (
-                <div
-                  key={index}
-                  className={`group p-4 mr-4 sm:p-6 rounded-2xl bg-transparent  hover:bg-accent hover:border-accent-foreground/20 transition-all duration-200`}
-                >
-                  <div className="flex flex-col items-center text-center space-y-3">
-                    <div
-                      className={``}
-                    >
-                      {tech.icon}
-                    </div>
-                    <div>
-                      <h3 className="text-sm sm:text-base mb-1 text-nowrap">
-                        {tech.name}
-                      </h3>
+              {/* Technology Grid */}
+              <motion.div
+                whileInView={{ opacity: 1 }}
+                initial={{ x: 0 }}
+                viewport={{ once: true }}
+                animate={{ x: "-100%" }}
+                transition={{
+                  repeat: Infinity,
+                  duration: 30,
+                  ease: "linear",
+                }}
+                className="flex"
+              >
+                {repeatTechnologies.map((tech, index) => (
+                  <div
+                    key={index}
+                    className={`group p-4 mr-4 sm:p-6 rounded-2xl bg-transparent  hover:bg-accent hover:border-accent-foreground/20 transition-all duration-200`}
+                  >
+                    <div className="flex flex-col items-center text-center space-y-3">
+                      <div className={``}>{tech.icon}</div>
+                      <div>
+                        <h3 className="text-sm sm:text-base mb-1 text-nowrap">
+                          {tech.name}
+                        </h3>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
-            </motion.div>
-            {/* Technology Grid */}
-            <motion.div
-              whileInView={{ opacity: 1}}
-              viewport={{ once: true}}
-              initial={{ x: 0 }}
-              animate={{ x: "-100%" }}
-              transition={{
-                repeat: Infinity,
-                duration: 30,
-                ease: "linear",
-              }}
-              className="flex"
-            >
-              {repeatTechnologies.map((tech, index) => (
-                <div
-                  key={index + 2}
-                  className={`group  mr-4 p-4 sm:p-6 rounded-2xl bg-transparent  hover:bg-accent hover:border-accent-foreground/20 transition-all duration-200`}
-                >
-                  <div className="flex flex-col items-center text-center space-y-3">
-                    <div
-                      className={``}
-                    >
-                      {tech.icon}
-                    </div>
-                    <div>
-                      <h3 className="text-sm sm:text-base mb-1 text-nowrap">
-                        {tech.name}
-                      </h3>
+                ))}
+              </motion.div>
+              {/* Technology Grid */}
+              <motion.div
+                whileInView={{ opacity: 1 }}
+                viewport={{ once: true }}
+                initial={{ x: 0 }}
+                animate={{ x: "-100%" }}
+                transition={{
+                  repeat: Infinity,
+                  duration: 30,
+                  ease: "linear",
+                }}
+                className="flex"
+              >
+                {repeatTechnologies.map((tech, index) => (
+                  <div
+                    key={index + 2}
+                    className={`group  mr-4 p-4 sm:p-6 rounded-2xl bg-transparent  hover:bg-accent hover:border-accent-foreground/20 transition-all duration-200`}
+                  >
+                    <div className="flex flex-col items-center text-center space-y-3">
+                      <div className={``}>{tech.icon}</div>
+                      <div>
+                        <h3 className="text-sm sm:text-base mb-1 text-nowrap">
+                          {tech.name}
+                        </h3>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
-            </motion.div>              
+                ))}
+              </motion.div>
             </div>
             <div className="flex brightness-75">
-            {/* Technology Grid reversed*/}
-            <motion.div
-              whileInView={{ opacity: 1 }}
-              initial={{ x: "-100%" }}
-              viewport={{ once: true}}
-              animate={{ x: 0 }}
-              transition={{
-                repeat: Infinity,
-                duration: 30,
-                ease: "linear",
-              }}
-              className="flex"
-            >
-              {repeatTechnologiesRev.map((tech, index) => (
-                <div
-                  key={`row-2-${index}`}
-                  className={`group p-4 mr-4 sm:p-6 rounded-2xl bg-transparent  hover:bg-accent hover:border-accent-foreground/20 transition-all duration-200`}
-                >
-                  <div className="flex flex-col items-center text-center space-y-3">
-                    <div
-                      className={``}
-                    >
-                      {tech.icon}
-                    </div>
-                    <div>
-                      <h3 className="text-sm sm:text-base mb-1 text-nowrap">
-                        {tech.name}
-                      </h3>
+              {/* Technology Grid reversed*/}
+              <motion.div
+                whileInView={{ opacity: 1 }}
+                initial={{ x: "-100%" }}
+                viewport={{ once: true }}
+                animate={{ x: 0 }}
+                transition={{
+                  repeat: Infinity,
+                  duration: 30,
+                  ease: "linear",
+                }}
+                className="flex"
+              >
+                {repeatTechnologiesRev.map((tech, index) => (
+                  <div
+                    key={`row-2-${index}`}
+                    className={`group p-4 mr-4 sm:p-6 rounded-2xl bg-transparent  hover:bg-accent hover:border-accent-foreground/20 transition-all duration-200`}
+                  >
+                    <div className="flex flex-col items-center text-center space-y-3">
+                      <div className={``}>{tech.icon}</div>
+                      <div>
+                        <h3 className="text-sm sm:text-base mb-1 text-nowrap">
+                          {tech.name}
+                        </h3>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
-            </motion.div>
-            {/* Technology Grid */}
-            <motion.div
-              whileInView={{ opacity: 1}}
-              viewport={{ once: true}}
-              initial={{ x: "-100%" }}
-              animate={{ x: 0 }}
-              transition={{
-                repeat: Infinity,
-                duration: 30,
-                ease: "linear",
-              }}
-              className="flex"
-            >
-              {repeatTechnologiesRev.map((tech, index) => (
-                <div
-                  key={index}
-                  className={`group  mr-4 p-4 sm:p-6 rounded-2xl bg-transparent  hover:bg-accent hover:border-accent-foreground/20 transition-all duration-200`}
-                >
-                  <div className="flex flex-col items-center text-center space-y-3">
-                    <div
-                      className={``}
-                    >
-                      {tech.icon}
-                    </div>
-                    <div>
-                      <h3 className="text-sm sm:text-base mb-1 text-nowrap">
-                        {tech.name}
-                      </h3>
+                ))}
+              </motion.div>
+              {/* Technology Grid */}
+              <motion.div
+                whileInView={{ opacity: 1 }}
+                viewport={{ once: true }}
+                initial={{ x: "-100%" }}
+                animate={{ x: 0 }}
+                transition={{
+                  repeat: Infinity,
+                  duration: 30,
+                  ease: "linear",
+                }}
+                className="flex"
+              >
+                {repeatTechnologiesRev.map((tech, index) => (
+                  <div
+                    key={index}
+                    className={`group  mr-4 p-4 sm:p-6 rounded-2xl bg-transparent  hover:bg-accent hover:border-accent-foreground/20 transition-all duration-200`}
+                  >
+                    <div className="flex flex-col items-center text-center space-y-3">
+                      <div className={``}>{tech.icon}</div>
+                      <div>
+                        <h3 className="text-sm sm:text-base mb-1 text-nowrap">
+                          {tech.name}
+                        </h3>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
-            </motion.div>              
+                ))}
+              </motion.div>
             </div>
           </div>
         </div>
